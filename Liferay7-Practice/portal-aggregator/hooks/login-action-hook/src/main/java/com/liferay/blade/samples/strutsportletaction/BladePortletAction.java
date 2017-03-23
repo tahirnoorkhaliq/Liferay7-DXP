4f2deb1430071eeefb20bd078ba5cd47cd01d633
tahirnoorkhaliq <tahirnoorkhaliq@gmail.com>
2017-03-23 15:00:30 +0530

    Update BladePortletAction.java

--- Liferay7-Practice/portal-aggregator/hooks/login-action-hook/src/main/java/com/liferay/blade/samples/strutsportletaction/BladePortletAction.java
+++ Liferay7-Practice/portal-aggregator/hooks/login-action-hook/src/main/java/com/liferay/blade/samples/strutsportletaction/BladePortletAction.java
@@ -1,16 +1,5 @@
-/**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- */
+
+
 
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
@@ -35,16 +24,14 @@ import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 import com.tahir.liferaypractice.service.TestServiceModuleLocalServiceUtil;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 
 
-
 /**
- * @author Liferay
+ * @author Tahir Noor Khaliq
  */
 @Component(
 		property = {
@@ -56,25 +43,37 @@ import org.osgi.service.component.annotations.Reference;
 public class BladePortletAction extends BaseMVCActionCommand {
 
 
-    @Reference(target = "(&(mvc.command.name=/login/login)" +
-                        "(javax.portlet.name=com_liferay_login_web_portlet_LoginPortlet)"+
-                        "(component.name=com.liferay.login.web.internal.portlet.action.LoginMVCActionCommand))")
-
 
      protected MVCActionCommand mvcActionCommand;
+
+    @Reference(target = "(&(mvc.command.name=/login/login)" +
+            "(javax.portlet.name=com_liferay_login_web_portlet_LoginPortlet)"+
+            "(component.name=com.liferay.login.web.internal.portlet.action.LoginMVCActionCommand))")
+    public void setMvcActionCommand(MVCActionCommand mvcActionCommand) {
+        this.mvcActionCommand = mvcActionCommand;
+    }
 
     @Override
     protected void doProcessAction(
             ActionRequest actionRequest, ActionResponse actionResponse)
             throws Exception {
-        String url = "http://www.google.com/search?q=httpClient";
         HttpClient client = HttpClientBuilder.create().build();
-        HttpGet request = new HttpGet(url);
 
-        System.out.println(">>>>>>>>>>>>>>>>>>>OVERRIDING MVCACTIONCOMMAND<<<<<<<<<<<<<<<<<<<<<< "+TestServiceModuleLocalServiceUtil.getTestServiceModulesCount());
+        System.out.println(">>>>>>>>>>>>>>>>>>>OVERRIDING MVCACTIONCOMMAND<<<<<<<<<<<<<<<<<<<<<< ");
         mvcActionCommand.processAction(actionRequest,actionResponse);
-        //actionResponse.sendRedirect("/web/guest/home");
+       // actionResponse.sendRedirect("/web/guest/home");
         //https://www.mkyong.com/java/apache-httpclient-examples/
+
+
+
     }
+
+    /* another way to add dependency in bnd file
+
+    Export-Package:  com.liferay.blade.samples.strutsportletaction
+-includeresource:\
+	@httpclient-[0-9]*.jar,\
+	@httpcore-[0-9]*.jar,\
+     */
 
 }
